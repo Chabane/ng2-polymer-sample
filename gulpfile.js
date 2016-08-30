30a8a897af569f61c5e1eb1d7729c1ad4b5ce7b3
System Administrator <root@iMac-de-Chabane.local>
2016-08-31 00:23:30 +0200

    rework deploying on prod

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var vulcanize = require('gulp-vulcanize');
 var crisper = require('gulp-crisper');
 var clean = require('gulp-clean');
 var runSequence = require('run-sequence');
+var htmlreplace = require('gulp-html-replace');
 
 gulp.task('vulcanize', function() {
   return gulp.src('dist/elements.html')
@@ -34,9 +35,17 @@ gulp.task('clean-elements', function () {
     .pipe(clean());
 });
 
-
+gulp.task('copy-polymer-dependencies', function() {
+  gulp.src('dist/index.html')
+    .pipe(htmlreplace({
+        'html': 'dist/elements.html',
+        'js': 'dist/elements.js',
+        'webcomponents': 'dist/webcomponents-lite.min.js'
+    }))
+    .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('default', function(callback) {
-  runSequence('vulcanize', 'move-webcomponentsjs', ['clean-elements-html', 'clean-bower-components'], 'move-elements', 'clean-elements',
+  runSequence('vulcanize', 'move-webcomponentsjs', ['clean-elements-html', 'clean-bower-components'], 'move-elements', 'clean-elements', 'copy-polymer-dependencies',
               callback);
 });
